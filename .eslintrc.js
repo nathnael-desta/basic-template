b3e2f23b08603cab91c806e94d107c514873d5f2
Nathnael Desta <destanathnaelataro@gmail.com>
2024-02-08 05:40:36 +0300

    add webpack,eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base',"some-other-config-you-use",
+  "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    "quotes": [
+      "error",
+      "double"
+  ]
+  },
+};
